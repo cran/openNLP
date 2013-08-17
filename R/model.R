5aa3b96c561b0d04028e37a8b426272b973cc652
Kurt Hornik <Kurt.Hornik@R-project.org>
2013-08-17 00:00:00 +0000

    version 0.2-0

--- R/model.R
+++
@@ -1,8 +0,0 @@
-getModel <-
-function(model)
-{
-    .jcall(.jnew("opennlp/maxent/io/SuffixSensitiveGISModelReader",
-                 .jnew("java.io.File", model)),
-           "Lopennlp/maxent/GISModel;",
-           "getModel")
-}
